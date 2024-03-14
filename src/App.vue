aa24d0f8da81a5729ffd0b9f8694fdca93801a74
neogen22 <nickomjotov@yandex.ru>
2024-03-14 08:10:04 +0300

    buttons size remastered

--- src/App.vue
+++ src/App.vue
@@ -11,14 +11,16 @@ import KonkortAbout from './components/KonkortAbout.vue'
     href="https://fonts.googleapis.com/css2?family=Prata&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
     rel="stylesheet"
   />
-  <div class="container mx-auto place-items-center max-w-5xl">
-    <div class="flex flex-column gap-12 mt-6">
-      <Button>О КОМПАНИИ</Button>
-      <Button>НОВОСТИ</Button>
+  <div class="container mx-auto place-items-center max-w-5xl mt-6">
+    <div class="flex flex-wrap justify-around gap-4">
+      <Button>&nbsp;&nbsp;О КОМПАНИИ&nbsp;&nbsp;</Button>
+      <Button
+        >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;НОВОСТИ&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</Button
+      >
       <Button>ВИДЕОНОВОСТИ</Button>
-      <Button>ТЕЛЕВИДЕНИЕ</Button>
-      <Button>АБОНЕНТАМ</Button>
-      <Button>КОНТАКТЫ</Button>
+      <Button>&nbsp;&nbsp;ТЕЛЕВИДЕНИЕ&nbsp;&nbsp;</Button>
+      <Button>&nbsp;&nbsp;&nbsp;&nbsp;АБОНЕНТАМ&nbsp;&nbsp;&nbsp;</Button>
+      <Button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;КОНТАКТЫ&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</Button>
     </div>
     <KonkortAbout></KonkortAbout>
   </div>
