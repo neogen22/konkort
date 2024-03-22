7654cfb20c56bcfcef4bb810a1d420ffeb6ddd5a
neogen22 <49803670+neogen22@users.noreply.github.com>
2024-03-22 11:01:56 +0300

    some changes in vav

--- src/App.vue
+++ src/App.vue
@@ -55,7 +55,7 @@ function z() {
               </p></NavigationMenuTrigger
             ><NavigationMenuContent>
               <ul>
-                <NavigationMenuLink href="/tv/connect"
+                <NavigationMenuLink @click="$router.push('/tv/connect')" asChild
                   ><p
                     class="touch-auto text-center w-[200px] p-2 cursor-pointer hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none transition ease-in-out delay-150"
                   >
