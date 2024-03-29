5301e4cf8ede7e96886d3aeb58cc28094b5dfae6
neogen22 <nickomjotov@yandex.ru>
2024-03-29 19:34:53 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@ import {
 import router from './router'
 </script>
 
-<template>
+<template v-cloak>
   <link
     href="https://fonts.googleapis.com/css2?family=Prata&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
     rel="stylesheet"
@@ -188,4 +188,8 @@ import router from './router'
   </body>
 </template>
 
-<style scoped></style>
+<style scoped>
+[v-cloak] {
+  display: none;
+}
+</style>
